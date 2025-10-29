7e67f25ad8e32537edb3ab5c3d6308f89d360677
Murilo Salvador Dalmonech <murilo.salvador95@gmail.com>
2025-10-29 01:03:42 -0300

    fix sub retro

--- substituicoes_Retroativas/substituicoes_retroativas.m
+++ substituicoes_Retroativas/substituicoes_retroativas.m
@@ -1,6 +1,6 @@
 function [x] = substituicoes_retroativas(n, L, c)
   x(n, 1) = c(n) / L(n,n);
-  for i=n-1:1:-1
+  for i=n-1:-1:1
     Soma = 0;
     for j=i+1:n
       Soma = Soma + L(i,j) * x(j, 1);
